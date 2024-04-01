13da3d4d25045a3c0caad8ba4f21e75f2d8a8257
felipe valeriano <felipevalerianodosreis@gmail.com>
2024-04-01 10:44:23 -0300

    first

---
+++ lib/telas/exercicio.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class Exercicio extends StatelessWidget {
+  const Exercicio({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 25,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
+        title: const Text(
+          "© Téchnotribe MLV ",
+          style: TextStyle(color: Colors.white, fontSize: 15),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 0, 98, 147),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 20.0),
+          Image.asset(
+            "assets/logo.png",
+            height: 180,
+          ),
+          Container(
+            width: 400,
+            height: 0.5,
+            color: Colors.grey,
+          ),
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                _buildInputWithIconAndPlaceholder(
+                  icon: Icons.email,
+                  placeholder: 'Cadastro de Pessoa Física',
+                ),
+                const SizedBox(height: 10.0), // Adicionando espaço entre os campos de texto
+                _buildInputWithIconAndPlaceholder(
+                  icon: Icons.lock,
+                  placeholder: 'Senha',
+                  obscureText: true,
+                ),
+                          const SizedBox(height: 20.0),
+                Row( // Adicionando a opção "Lembrar senha"
+                  children: [
+                    Checkbox(
+                      value: true, // Defina o valor para verdadeiro se desejar que esteja marcado por padrão
+                      onChanged: (bool? value) {
+                        // Lógica para lembrar ou não a senha
+                      },
+                    ),
+                    Text(
+                      'Lembrar senha',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle login button press
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 0, 98, 147)), // cor de fundo do botão
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0), // Aumentando o raio da borda
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0), // Aumentando o padding interno
+                    ),
+                  ),
+                  child: Text('Iniciar Sessão', style: TextStyle(fontSize: 18, color: Colors.white)),
+                ),
+                const SizedBox(height: 20.0),
+                GestureDetector( // Adicionando o texto "Esqueci a senha"
+                  onTap: () {
+                    // Lógica para redirecionar para a página de recuperação de senha
+                  },
+                  child: Text(
+                    'Esqueci a senha',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInputWithIconAndPlaceholder({
+    required IconData icon,
+    required String placeholder,
+    bool obscureText = false,
+  }) {
+    return Container(
+      width: 400,
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.grey[200],
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 64, 127, 179),
+            ),
+            child: Icon(
+              icon,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(width: 15.0),
+          Expanded(
+            child: TextField(
+              obscureText: obscureText,
+              decoration: InputDecoration(
+                hintText: placeholder,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
